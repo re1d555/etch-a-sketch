b49a79c13cfc763904a4f5c86073811dfc64bfb9
re1d555 <55630923+re1d555@users.noreply.github.com>
2024-01-09 22:47:31 +0300

    Add select

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const gridsDiv = document.querySelector('.grids');
+const selector = document.querySelector('#gridSelect');
 
-for (i = 0; i < 16; i++) {
-const grid = document.createElement('div');
-grid.className = 'grid';
-gridsDiv.appendChild(grid);
-}
+
+selector.addEventListener('change', (e) =>{
+    let selectOption = e.target.value
+    // console.log(selectOption);
+    // switch (selectOption) {
+    //   case 1:
+    //   
+    // }
+for (i = 0; i < selectOption; i++) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+    grid.style.cssText += 'width: 25px; height: 25px';
+    gridsDiv.appendChild(grid);
+    }
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseenter', (e) =>{
+        e.target.style.background = colorChange();
+    }));
+  })
+
 
 function randomColor() {
     return random = (Math.floor(Math.random() * 255));
 }
+
+function colorChange() {
+    return rndColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+}
+
 
