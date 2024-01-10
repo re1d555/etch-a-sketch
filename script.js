5b750650b80c8e4f17844658fc792c1f679d7f4c
re1d555 <55630923+re1d555@users.noreply.github.com>
2024-01-10 19:34:56 +0300

    Add button that calls a prompt

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 const gridsDiv = document.querySelector('.grids');
-const selector = document.querySelector('#gridSelect');
-
-
-selector.addEventListener('change', (e) =>{
-    let selectOption = e.target.value
-    // console.log(selectOption);
-    // switch (selectOption) {
-    //   case 1:
-    //   
-    // }
-for (i = 0; i < selectOption; i++) {
-    const grid = document.createElement('div');
-    grid.className = 'grid';
-    grid.style.cssText += 'width: 25px; height: 25px';
-    gridsDiv.appendChild(grid);
-    }
-    const grids = document.querySelectorAll('.grid');
-    grids.forEach(grid => grid.addEventListener('mouseenter', (e) =>{
-        e.target.style.background = colorChange();
-    }));
-  })
-
+const selectButton = document.querySelector('.gridSelect');
 
 function randomColor() {
     return random = (Math.floor(Math.random() * 255));
@@ -29,5 +8,28 @@ function randomColor() {
 function colorChange() {
     return rndColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
 }
+
+function play() {
+    const createdGrids = document.querySelectorAll('.grid');
+    createdGrids.forEach(createdGrid => createdGrid.remove());
+
+    let gridQuantity = prompt('Choose field, from 1 to 100', 1) ;
+    let gridQuantityAll = gridQuantity ** 2;
+
+    for (i = 0; i < gridQuantityAll; i++) {
+        let grid = document.createElement('div');
+        grid.className = 'grid';
+        grid.style.cssText += `width: ${400 / gridQuantity}px; height: ${400 / gridQuantity}px; background-color: white;`
+        gridsDiv.appendChild(grid);
+    }
+
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseenter', (e) =>{
+        e.target.style.background = colorChange();
+    }));
+
+}
+
+selectButton.addEventListener('click', play);
 
 
