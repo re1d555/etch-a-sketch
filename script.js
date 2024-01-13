e2fe52355b6f1ad0f79a80ff97089711990d5708
re1d555 <55630923+re1d555@users.noreply.github.com>
2024-01-13 17:51:51 +0300

    Add shadow mod and events killer
    shadow mod didn't correctly work with black and rgb mods
    had to add events killer, code is garbage fuck my life

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
 const gridsDiv = document.querySelector('.grids');
 const selectButton = document.querySelector('.gridSelect');
 const clearButton = document.querySelector('.clear');
-const standard = document.querySelector('.standard');
+const black = document.querySelector('.standard');
 const rgb = document.querySelector('.rgb');
 const darkMode = document.querySelector('.darker');
 const modeBtn = document.querySelectorAll('.modBtn').forEach((btn) => btn.disabled = true);
+
+function clear() {
+    const clearGrids = document.querySelectorAll('.grid');
+    clearGrids.forEach(grid => grid.style.background = 'rgb(255, 255, 255)');
+}
+
+function shadowMode() {
+    killEventListeners()
+    black.style.cssText = '';
+    rgb.style.cssText = '';
+    darkMode.style.cssText = 'background-color: grey; color: white';
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.style.background = `rgb(255, 255, 255)`);
+    grids.forEach(grid => grid.addEventListener('mouseenter', (e) =>{
+        let currentColor = e.target.style.background.slice(14, -1);
+        let nextColor = currentColor - currentColor * 0.1;
+        e.target.style.background = `rgb(${nextColor}, ${nextColor}, ${nextColor})`
+    }));
+}
+
+function killEventListeners() {
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.removeEventListener('mouseenter', blackModeEventListener));
+    grids.forEach(grid => grid.removeEventListener('mouseenter', rgbModeEventListener));
+}
+
+function addEventListeners(handler) {
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseenter', handler));    
+}
+
+function blackMode() {
+    black.style.cssText = 'background-color: grey; color: white';
+    rgb.style.cssText = '';
+    darkMode.style.cssText = '';
+    addEventListeners(blackModeEventListener)
+}
+
+function blackModeEventListener(e) {
+    e.target.style.background = 'rgb(0, 0, 0)';
+}
+
+function rgbMode() {
+    black.style.cssText = '';
+    rgb.style.cssText = 'background-color: grey; color: white';
+    darkMode.style.cssText = '';
+    addEventListeners(rgbModeEventListener);
+}
+
+function rgbModeEventListener(e) {
+    e.target.style.background = colorChange(255);
+}
 
 function randomColor(mode) {
     return random = (Math.floor(Math.random() * mode));
@@ -14,68 +66,35 @@ function colorChange(color) {
     return rndColor = `rgb(${randomColor(color)}, ${randomColor(color)}, ${randomColor(color)})`;
 }
 
-function modSelection(color) {
-    let grids = document.querySelectorAll('.grid');
-    grids.forEach(grid => grid.addEventListener('mouseenter', (e) =>{
-            e.target.style.background = colorChange(color);
-    }));
-}
-
-function clear() {
-    const createdGrids = document.querySelectorAll('.grid');
-    createdGrids.forEach(grid => grid.style.background = 'white');
-}
-
 function gridQuantity() {
-
+// refresh the field
     const createdGrids = document.querySelectorAll('.grid');
     createdGrids.forEach(createdGrid => createdGrid.remove());
 
     let gridQuantity = prompt('Choose field, from 1 to 100', 1) ;
-
     if (gridQuantity > 0 && gridQuantity <= 100) {
-
+// create the field
         let gridQuantityAll = gridQuantity ** 2;
-
         for (i = 0; i < gridQuantityAll; i++) {
             let grid = document.createElement('div');
             grid.className = 'grid';
-            grid.style.cssText += `width: ${400 / gridQuantity}px; height: ${400 / gridQuantity}px; background-color: white;`
+            grid.style.cssText += `width: ${500 / gridQuantity}px; height: ${500 / gridQuantity}px; background-color: rgb(255, 255, 255); transition: 0.6s; border-radius: 7px`;
             gridsDiv.appendChild(grid);
         }
-
-        modSelection(0)
-
+// enable mode buttons
         document.querySelectorAll('.modBtn').forEach((btn) => btn.disabled = false);
-        standard.style.cssText = 'background-color: grey; color: white';
-        rgb.style.cssText = '';
-        darkMode.style.cssText = '';
 
     } else {
         alert('PLEASE! write number from 1 to 100');
+        document.querySelectorAll('.modBtn').forEach((btn) => btn.disabled = true);
+        black.style.cssText = '';
+        rgb.style.cssText = '';
+        darkMode.style.cssText = '';
     }
 }
 
 selectButton.addEventListener('click', gridQuantity);
 clearButton.addEventListener('click', clear);
-standard.addEventListener('click', () => {
-    modSelection(0);
-    standard.style.cssText = 'background-color: grey; color: white';
-    rgb.style.cssText = '';
-    darkMode.style.cssText = '';
-});
-rgb.addEventListener('click', () => {
-    modSelection(255)
-    standard.style.cssText = '';
-    rgb.style.cssText = 'background-color: grey; color: white';
-    darkMode.style.cssText = '';
-});
-
-// darkMode.addEventListener('click', () => {
-//     modSelection(255);
-//     standard.style.cssText = '';
-//     rgb.style.cssText = '';
-//     darkMode.style.cssText = 'background-color: grey; color: white';
-
-// })
-
+black.addEventListener('click', blackMode);
+rgb.addEventListener('click', rgbMode);
+darkMode.addEventListener('click', shadowMode);
